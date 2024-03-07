450636184b44090e43b61298fcc4138e6bfae2b9
Stephen Isienyi <stephen.isienyi@gmail.com>
2024-03-07 12:03:48 -0800

    update source base

--- src/index.js
+++ src/index.js
@@ -5,3 +5,17 @@ export {
     getClientStorage,
     getServerStorage
 } from '@webkrafters/universal-storage';
+
+/** @typedef {import("@webkrafters/universal-storage").Id} Id */
+/**
+ * @typedef {import("@webkrafters/universal-storage").LocalStorageType<P>} LocalStorageType
+ * @template [P={}]
+ */
+/** @typedef {import("@webkrafters/universal-storage").ParsedCookies} ParsedCookies */
+/** @typedef {import("@webkrafters/universal-storage").RawCookies} RawCookies */
+/**
+ * @typedef {import("@webkrafters/universal-storage").Request<CDATA>} Request
+ * @template {Cookies} [CDATA=Cookies]
+ */
+/** @typedef {import("@webkrafters/universal-storage").Response} Response */
+/** @typedef {import("@webkrafters/universal-storage").Cookies} Cookies */
